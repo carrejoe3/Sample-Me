5481946b495a5a3c149f8615bf09e5194e6b1bd8
Joe <carre.joe3@gmail.com>
2019-12-31 14:51:11 +0000

    replaced helloworld component with sampler component

--- sampleme/src/views/Home.vue
+++ sampleme/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sampler/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Sampler from '@/components/Sampler.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Sampler
   }
 }
 </script>
