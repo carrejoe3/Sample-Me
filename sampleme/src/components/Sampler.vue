0e96190649feea386739b0387a11bf356bd1a40e
Joe <carre.joe3@gmail.com>
2019-12-31 15:13:15 +0000

    added some button rows

--- sampleme/src/components/Sampler.vue
+++ sampleme/src/components/Sampler.vue
@@ -1,5 +1,25 @@
 <template>
   <v-container>
+    <v-row no-gutters>
+      <v-col v-for="n in 3" :key="n" cols="4" xs="4">
+        <v-card class="pa-2" outlined tile>One of three columns</v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col v-for="n in 3" :key="n" cols="4" xs="4">
+        <v-card class="pa-2" outlined tile>One of three columns</v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col v-for="n in 3" :key="n" cols="4" xs="4">
+        <v-card class="pa-2" outlined tile>One of three columns</v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col v-for="n in 3" :key="n" cols="4" xs="4">
+        <v-card class="pa-2" outlined tile>One of three columns</v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
